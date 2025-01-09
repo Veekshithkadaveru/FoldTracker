1cd7c1fcbce4865b2e7eb210ce8a7bf5ed2a64e8
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2025-01-09 19:15:34 +0530

    Setup DI

---
+++ app/src/main/java/com/example/foldtracker/di/AppModule.kt
@@ -0,0 +1,24 @@
+package com.example.foldtracker.di
+
+import android.content.Context
+import com.example.foldtracker.repository.CounterRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideCounterRepository(
+        @ApplicationContext context: Context
+    ): CounterRepository {
+        return CounterRepository(context)
+    }
+}

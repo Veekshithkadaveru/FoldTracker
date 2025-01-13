8953dd91763c137eb37212c159bce1a9b203d714
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2025-01-13 22:26:16 +0530

    Create Counter Screen

--- app/src/main/java/com/example/foldtracker/ui/CounterScreen.kt
+++ app/src/main/java/com/example/foldtracker/ui/CounterScreen.kt
@@ -1,2 +1,30 @@
 package com.example.foldtracker.ui
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.foldtracker.viewmodel.CounterViewModel
+
+@Composable
+fun CounterScreen(viewModel: CounterViewModel) {
+
+    val counter by viewModel.counter.collectAsState()
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Fold Count:$counter", style = MaterialTheme.typography.bodyMedium)
+    }
+}

f1a39864379eb8c5678163b7b8f1d1b0fedcdc4a
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2025-01-27 18:50:25 +0530

    Add DataStore keys for counter, daily count, and last updated date

---
+++ app/src/main/java/com/example/foldtracker/datastore/DataStoreKeys.kt
@@ -0,0 +1,10 @@
+package com.example.foldtracker.datastore
+
+import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.core.stringPreferencesKey
+
+object DataStoreKeys {
+    val COUNTER_KEY = intPreferencesKey("counter_key")
+    val DAILY_COUNT_KEY = intPreferencesKey("daily_count_key")
+    val LAST_UPDATED_DATE_KEY = stringPreferencesKey("last_updated_date_key")
+}

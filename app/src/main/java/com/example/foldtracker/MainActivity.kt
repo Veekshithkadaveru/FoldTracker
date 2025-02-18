528e902aec7db7582a95a4b42949fd3112d245c0
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2025-02-18 22:04:55 +0530

    Start Foreground Service on launch

--- app/src/main/java/com/example/foldtracker/MainActivity.kt
+++ app/src/main/java/com/example/foldtracker/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.foldtracker
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
@@ -10,9 +11,11 @@ import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.window.layout.FoldingFeature
 import androidx.window.layout.WindowInfoTracker
+import com.example.foldtracker.service.FoldTrackerService
 import com.example.foldtracker.ui.navigation.AppNavigation
 import com.example.foldtracker.viewmodel.CounterViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -23,6 +26,7 @@ class MainActivity : ComponentActivity() {
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        startFoldTrackingService()
         setContent {
             val viewModel: CounterViewModel = hiltViewModel()
 
@@ -76,4 +80,9 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun startFoldTrackingService() {
+        val serviceIntent = Intent(this, FoldTrackerService::class.java)
+        ContextCompat.startForegroundService(this, serviceIntent)
+    }
 }

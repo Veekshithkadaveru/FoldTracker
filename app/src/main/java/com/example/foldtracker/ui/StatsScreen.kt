25095b394ab106ff9bdc49911fa775d49bc4618e
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2025-03-05 18:18:27 +0530

    Add Stats screen in the app navigation

---
+++ app/src/main/java/com/example/foldtracker/ui/StatsScreen.kt
@@ -0,0 +1,11 @@
+package com.example.foldtracker.ui
+
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ExtraFeatures(){
+    LazyVerticalGrid(columns = 2) {
+        
+    }
+}
--- app/src/main/java/com/example/foldtracker/ui/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/foldtracker/ui/navigation/AppNavigation.kt
@@ -1,14 +1,18 @@
 package com.example.foldtracker.ui.navigation
 
 import android.annotation.SuppressLint
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
 
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.foldtracker.ui.CounterScreen
+import com.example.foldtracker.ui.StatsScreen
 import com.example.foldtracker.viewmodel.CounterViewModel
 
+@RequiresApi(Build.VERSION_CODES.O)
 @SuppressLint("StateFlowValueCalledInComposition")
 @Composable
 fun AppNavigation(viewModel: CounterViewModel) {
@@ -16,8 +20,11 @@ fun AppNavigation(viewModel: CounterViewModel) {
 
     NavHost(navController = navController, startDestination = "counter_screen") {
         composable("counter_screen") {
-            CounterScreen(viewModel = viewModel)
+            CounterScreen(viewModel = viewModel,navController)
         }
 
+        composable("stats_screen"){
+            StatsScreen(viewModel = viewModel)
+        }
     }
 }
